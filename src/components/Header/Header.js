adc8b6a3531de23b2d62234698d84a50607bb06d
Ahmed M. Ezzeldeen <Ahmed.Mohamed.Ezzeldeen@gmail.com>
2023-05-18 06:40:48 +0300

    Added Header Section Just For Desktop

---
+++ src/components/Header/Header.js
@@ -0,0 +1,23 @@
+import './Header.css';
+import { TiShoppingCart } from "react-icons/ti";
+
+function Header() {
+  return (
+    <header className="main-header">
+      <h1 className="logo" >Your<span>Car</span></h1>
+      <nav className="main-nav" >
+        <ul className="main-link-list">
+          <li><a href="/#Home" className="active">Home</a></li>
+          <li><a href="/#About" >About</a></li>
+          <li><a href="/#Services" >Services</a></li>
+          <li><a href="/#Cars" >Cars</a></li>
+          <li><a href="/#Contact-us" >Contact us</a></li>
+        </ul>
+        <span className="shopping-icon"><TiShoppingCart /></span>
+      </nav>
+
+    </header>
+  );
+}
+
+export default Header;
